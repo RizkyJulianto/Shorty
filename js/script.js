f58a21b1668660d06654cc7bfe7c3288a19a7f65
Juliantomaga <juliantorizky01@gmail.com>
2024-10-04 10:24:50 +0700

    make price content

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ function showMenu() {
 function closeMenu() {
     navigasi.style.right = "-250px";
 }
+
+// Navbar Scroll Background 
+
+window.addEventListener('scroll' , function() {
+
+    const navBar = document.getElementById('navigation-bar');
+    const ScrollY = window.pageYOffset;
+    if(ScrollY > 5) {
+        navBar.classList.add('scroll-navbar');
+    } else {
+        navBar.classList.remove('scroll-navbar');
+    }
+})
