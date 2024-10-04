5155357212cff4141505167cd7633e17679004a4
Juliantomaga <juliantorizky01@gmail.com>
2024-10-04 08:28:23 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Show Menu Navbar
+const navigasi = document.getElementById('navigation-links');
+
+function showMenu() {
+    navigasi.style.right = "0";
+}
+
+function closeMenu() {
+    navigasi.style.right = "-250px";
+}
